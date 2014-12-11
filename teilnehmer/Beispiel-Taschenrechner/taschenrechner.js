aa1262edb846db140c76e2c676e0dac0d6cc6703
Johannes Link <business@johanneslink.net>
2014-12-11 15:45:16 +0100

    Taschenrechner: Zifferntasten

--- teilnehmer/Beispiel-Taschenrechner/taschenrechner.js
+++ teilnehmer/Beispiel-Taschenrechner/taschenrechner.js
@@ -1,10 +1,41 @@
 initialize();
 
+var displayText;
+
 function initialize() {
-  var result = document.getElementById("result");
-  result.innerHTML = "0";
-  var clearButton = document.getElementById("clearButton");
-  clearButton.onclick = function() {
-    result.innerHTML = "";
+  clearClicked();
+  document.getElementById("clearButton").onclick = clearClicked;
+  document.getElementById("plusButton").onclick = plusClicked;
+  document.getElementById("evalButton").onclick = evalClicked;
+  var digits = document.getElementsByClassName("digit");
+  for (var i = 0; i < digits.length; i++) {
+    var digitSpan = digits[i];
+    digitSpan.onclick = function(event) {
+      var digit = event.target.innerHTML;
+      digitClicked(digit);
+    }
   }
+}
+
+function clearClicked() {
+  displayText = "";
+  updateDisplay();
+}
+
+function plusClicked() {
+
+}
+
+function evalClicked() {
+
+}
+
+function digitClicked(digit) {
+  displayText += digit;
+  updateDisplay();
+}
+
+function updateDisplay() {
+  var display = document.getElementById("display");
+  display.innerHTML = displayText;
 }
