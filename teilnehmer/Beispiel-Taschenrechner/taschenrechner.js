f281586c2e55956f1e95ceba524667918e3879ff
Johannes Link <business@johanneslink.net>
2014-12-11 16:48:14 +0100

    clean up

--- teilnehmer/Beispiel-Taschenrechner/taschenrechner.js
+++ teilnehmer/Beispiel-Taschenrechner/taschenrechner.js
@@ -5,66 +5,66 @@ var storedNumber = 0;
 var wasNumberStored = false;
 
 function initialize() {
-    //resetCalculator();
-    document.getElementById("clearButton").onclick = clearClicked;
-    document.getElementById("evalButton").onclick = evalClicked;
-    document.getElementById("plusButton").onclick = plusClicked;
-    var digits = document.getElementsByClassName("digit");
-    for (var i = 0; i < digits.length; i++) {
-        var digitSpan = digits[i];
-        digitSpan.onclick = function (event) {
-            var digit = event.target.innerHTML;
-            digitClicked(digit);
-        }
+  resetCalculator();
+  document.getElementById("clearButton").onclick = clearClicked;
+  document.getElementById("evalButton").onclick = evalClicked;
+  document.getElementById("plusButton").onclick = plusClicked;
+  var digits = document.getElementsByClassName("digit");
+  for (var i = 0; i < digits.length; i++) {
+    var digitSpan = digits[i];
+    digitSpan.onclick = function (event) {
+      var digit = event.target.innerHTML;
+      digitClicked(digit);
     }
+  }
 }
 
 function resetCalculator() {
-    displayText = "";
-    storedNumber = 0;
-    wasNumberStored = false;
-    updateDisplay();
+  displayText = "";
+  storedNumber = 0;
+  wasNumberStored = false;
+  updateDisplay();
 }
 
 function clearClicked() {
-    console.log("C");
-    resetCalculator();
+  console.log("C");
+  resetCalculator();
 }
 
 function plusClicked() {
-    console.log("+");
-    evaluateWithPlus();
+  console.log("+");
+  evaluateWithPlus();
 }
 
 function evaluateWithPlus() {
-    if (wasNumberStored)
-        return;
-    var displayedNumber = parseInt(displayText);
-    storedNumber = storedNumber + displayedNumber;
-    wasNumberStored = true;
-    displayText = storedNumber.toString();
-    updateDisplay();
+  if (wasNumberStored)
+    return;
+  var displayedNumber = parseInt(displayText);
+  storedNumber = storedNumber + displayedNumber;
+  wasNumberStored = true;
+  displayText = storedNumber.toString();
+  updateDisplay();
 }
 
 function evalClicked() {
-    console.log("=");
-    evaluateWithPlus();
-    storedNumber = 0;
+  console.log("=");
+  evaluateWithPlus();
+  storedNumber = 0;
 }
 
 function digitClicked(digit) {
-    console.log(digit);
-    if (wasNumberStored) {
-        displayText = "";
-        wasNumberStored = false;
-    }
-    if (displayText.length == 10)
-        return;
-    displayText += digit;
-    updateDisplay();
+  console.log(digit);
+  if (wasNumberStored) {
+    displayText = "";
+    wasNumberStored = false;
+  }
+  if (displayText.length == 10)
+    return;
+  displayText += digit;
+  updateDisplay();
 }
 
 function updateDisplay() {
-    var display = document.getElementById("display");
-    display.innerHTML = displayText;
+  var display = document.getElementById("display");
+  display.innerHTML = displayText;
 }
